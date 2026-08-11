3be20084857bc74fe062213c8e80b0ae0aebb29e
Anushka-Nilakh <nilakhanushka26@gmail.com>
2026-08-11 16:14:58 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        if(x<0){
+            return false;
+        }
+        int rev=0;
+        while(x>0){
+           rev=rev*10+(x%10);
+           x=x/10;
+        }
+        if(num==rev){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
